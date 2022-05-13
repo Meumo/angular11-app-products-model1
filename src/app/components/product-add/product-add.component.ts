0b001904d4ef33388e3f593fb89d28c69ae518f6
Meumo <mamadou-d@hotmail.com>
2022-05-13 18:50:23 +0000

    First commit

---
+++ src/app/components/product-add/product-add.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ProductService} from "../../services/product.service";
+
+@Component({
+  selector: 'app-product-add',
+  templateUrl: './product-add.component.html',
+  styleUrls: ['./product-add.component.css']
+})
+export class ProductAddComponent implements OnInit {
+
+  productFormGroup!:FormGroup;
+  submitted:boolean=false;
+  constructor(private fb:FormBuilder, private productService:ProductService) { }
+
+  ngOnInit(): void {
+    this.productFormGroup=this.fb.group(
+      {
+        name:["",Validators.required],
+        price:[0,Validators.required],
+        quantity:[0,Validators.required],
+        selected:[true,Validators.required],
+        available:[true,Validators.required]
+      }
+    );
+  }
+
+  onSave() {
+    this.submitted=true;
+    if(this.productFormGroup.invalid) return;
+    this.productService.save(this.productFormGroup!.value)
+      .subscribe(data=>{
+        alert("Success saving Opertaion .....")
+      },error => {
+        console.log(error);
+      });
+  }
+}
---
+++ src/app/components/product-edit/product-edit.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {ProductService} from "../../services/product.service";
+import {Product} from "../../model/product.model";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-product-edit',
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.css']
+})
+export class ProductEditComponent implements OnInit {
+
+  productId: number;
+  productFormGroup!: FormGroup;
+  submitted: boolean = false;
+
+  constructor(private activatedRoute: ActivatedRoute, private productService: ProductService, private fb: FormBuilder, private router:Router) {
+    this.productId = this.activatedRoute.snapshot.params['id'];
+  }
+
+  ngOnInit(): void {
+    this.productService.getProducts(this.productId)
+      .subscribe(product => {
+        this.productFormGroup = this.fb.group({
+          id: [product.id, Validators.required],
+          name: [product.name, Validators.required],
+          price: [product.price, Validators.required],
+          quantity: [product.quantity, Validators.required],
+          selected: [product.selected, Validators.required],
+          available: [product.available, Validators.required]
+        })
+      });
+  }
+
+  onUpdate() {
+    this.productService.updateProduct(this.productFormGroup.value)
+      .subscribe(data => {
+        alert("Success");
+        this.router.navigateByUrl("/products");
+      }, error => {
+        console.log(error)
+      });
+  }
+}
